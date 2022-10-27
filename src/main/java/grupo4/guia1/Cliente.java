9b196b1c3dbe641d378afa5acd7ffb2d40a0c363
Igor Bohme <58749465+Zilenox@users.noreply.github.com>
2022-10-26 23:36:14 -0300

    Update Cliente.java

--- src/main/java/grupo4/guia1/Cliente.java
+++ src/main/java/grupo4/guia1/Cliente.java
@@ -13,6 +13,13 @@ public class Cliente {
     private String Nombre;
     private boolean Vigente;
 
+    public Cliente(String Cedula, String Nombre)
+    {
+        setCedula(Cedula);
+        setNombre(Nombre);
+        setVigente(true);
+    }
+    
     /**
      * @return the Cedula
      */
@@ -24,6 +31,15 @@ public class Cliente {
      * @param Cedula the Cedula to set
      */
     public void setCedula(String Cedula) {
+        if (Cedula.length() != 10) {
+            //throw new IllegalArgumentException("EL LARGO DE LA PATENTE DEBE SER DE 8");
+            System.out.println("EL LARGO DE LA CEDULA TIENE QUE SER DE 10 CARACTERES");
+            return;
+        }
+        if(!check_rut(Cedula)){
+            System.out.println("EL FORMATO DE LA CEDULA ES INCORRECTO");
+            return;
+        }
         this.Cedula = Cedula;
     }
 
@@ -55,4 +71,27 @@ public class Cliente {
         this.Vigente = Vigente;
     }
     
+    
+    private boolean check_rut(String str_input){
+        char[] str_in_array = str_input.toCharArray();
+        
+        
+        
+        if(str_in_array[8] != '-')
+            return false;
+        else if (!(str_in_array[9] > '0' && str_in_array[9] <= '9'))  
+        {
+            if(str_in_array[9] != 'k' && str_in_array[9] != 'K')
+                return false;
+        }
+        for (int i=0; i< 8; i++)
+        {
+            if(str_in_array[i] < '0' || str_in_array[i] > '9')
+            {
+                return false;
+            }
+        }
+        
+        return true;
+    }
 }
