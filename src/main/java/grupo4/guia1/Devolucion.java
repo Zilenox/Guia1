26665fd8eae16b4521f04024d2a4ea096415b61f
Igor Bohme <58749465+Zilenox@users.noreply.github.com>
2022-10-28 00:22:19 -0300

    Update Devolucion.java
    Se crea la clase Devolución, esta ligada a la clase Arriendo con una relaciona  de composición. se agregan los métodos

--- src/main/java/grupo4/guia1/Devolucion.java
+++ src/main/java/grupo4/guia1/Devolucion.java
@@ -3,87 +3,35 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package grupo4.guia1;
+import java.util.Calendar;
+
 
 /**
  *
  * @author ibohm
  */
 public class Devolucion {
-   private String Patente;
-   private String Marca;
-   private String Modelo;
-   private int anno;
-   private char Condicion;
-
-    /**
-     * @return the Patente
-     */
-    public String getPatente() {
-        return Patente;
-    }
-
-    /**
-     * @param Patente the Patente to set
-     */
-    public void setPatente(String Patente) {
-        this.Patente = Patente;
-    }
-
-    /**
-     * @return the Marca
-     */
-    public String getMarca() {
-        return Marca;
-    }
-
-    /**
-     * @param Marca the Marca to set
-     */
-    public void setMarca(String Marca) {
-        this.Marca = Marca;
-    }
-
-    /**
-     * @return the Modelo
-     */
-    public String getModelo() {
-        return Modelo;
-    }
-
-    /**
-     * @param Modelo the Modelo to set
-     */
-    public void setModelo(String Modelo) {
-        this.Modelo = Modelo;
-    }
-
-    /**
-     * @return the anno
-     */
-    public int getAnno() {
-        return anno;
-    }
-
-    /**
-     * @param anno the anno to set
-     */
-    public void setAnno(int anno) {
-        this.anno = anno;
-    }
-
-    /**
-     * @return the Condicion
-     */
-    public char getCondicion() {
-        return Condicion;
-    }
-
-    /**
-     * @param Condicion the Condicion to set
-     */
-    public void setCondicion(char Condicion) {
-        this.Condicion = Condicion;
-    }
    
-   
+    private Calendar FechaEntrega; 
+    
+    public Devolucion(Calendar FechaEntrega)
+    {
+        setFechaEntrega(FechaEntrega);
+    }
+
+    /**
+     * @return the FechaEntrega
+     */
+    public Calendar getFechaEntrega() {
+        return FechaEntrega;
+    }
+
+    /**
+     * @param FechaEntrega the FechaEntrega to set
+     */
+    public void setFechaEntrega(Calendar FechaEntrega) {
+        this.FechaEntrega = FechaEntrega;
+    }
+
+
 }
