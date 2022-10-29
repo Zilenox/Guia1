d1658fbe8406f5c9a1bc348503357f5838cc82fc
Igor Bohme <58749465+Zilenox@users.noreply.github.com>
2022-10-29 02:41:06 -0300

    Update Devolucion.java
    final

--- src/main/java/grupo4/guia1/Devolucion.java
+++ src/main/java/grupo4/guia1/Devolucion.java
@@ -13,12 +13,28 @@ import java.util.Calendar;
 public class Devolucion {
    
     private Calendar FechaEntrega; 
-    
-    public Devolucion(Calendar FechaEntrega)
+    private Arriendo arriendo;
+    public Devolucion(Arriendo arriendo, Calendar FechaEntrega)
     {
+        setArriendo(arriendo);
         setFechaEntrega(FechaEntrega);
+        
     }
 
+    private boolean ValidarFechaEntrega(Calendar fecha){
+        if(getArriendo().getFechaArriendo().compareTo(fecha) >0){
+            DEVOLUCION_messages("Fecha de devolucion no puede ser anterior a la fecha de arriendo");
+            return false;
+        }
+        return true;
+    }
+    public boolean ValidarDevolucion(Vehiculo v){
+        if(getArriendo().getVehiculo().equals(v)){
+            getArriendo().getVehiculo().setCondicion('D');
+            return true;
+        }
+        return false;
+    }
     /**
      * @return the FechaEntrega
      */
@@ -30,8 +46,27 @@ public class Devolucion {
      * @param FechaEntrega the FechaEntrega to set
      */
     public void setFechaEntrega(Calendar FechaEntrega) {
+        if(!ValidarFechaEntrega(FechaEntrega)){
+            return;
+        }
         this.FechaEntrega = FechaEntrega;
     }
 
+    /**
+     * @return the arriendo
+     */
+    private Arriendo getArriendo() {
+        return arriendo;
+    }
 
+    /**
+     * @param arriendo the arriendo to set
+     */
+    private void setArriendo(Arriendo arriendo) {
+        this.arriendo = arriendo;
+    }
+
+    private void DEVOLUCION_messages(String data){
+        System.out.println(data);
+    }
 }
